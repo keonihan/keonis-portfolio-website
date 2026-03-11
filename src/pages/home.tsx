df7543c976e38ec40a60ff9fb47ddcd7b83129d2
Keoni <keonihan@gmail.com>
2026-03-11 11:53:50 -1000

    Change the desktop mobile requirements

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,16 +2,32 @@ import { useEffect, useState } from "react";
 import DesktopPage from "../pages/DesktopHome";
 import MobilePage from "../pages/MobileHome";
 
+// export default function Home() {
+//   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+//   useEffect(() => {
+//     const handleResize = () => {
+//       setIsMobile(window.innerWidth < 768);
+//     };
+
+//     window.addEventListener("resize", handleResize);
+//     return () => window.removeEventListener("resize", handleResize);
+//   }, []);
+
+//   return isMobile ? <MobilePage /> : <DesktopPage />;
+// }
+
 export default function Home() {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  const [isMobile, setIsMobile] = useState(
+    window.matchMedia("(pointer: coarse)").matches
+  );
 
   useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
+    const media = window.matchMedia("(pointer: coarse)");
+    const listener = () => setIsMobile(media.matches);
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
+    media.addEventListener("change", listener);
+    return () => media.removeEventListener("change", listener);
   }, []);
 
   return isMobile ? <MobilePage /> : <DesktopPage />;
