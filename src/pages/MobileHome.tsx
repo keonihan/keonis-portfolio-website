4eededfb8049264176c13cd354e5180b16a40d26
Keoni <keonihan@gmail.com>
2026-03-10 20:14:39 -1000

    Fix on safari

--- src/pages/MobileHome.tsx
+++ src/pages/MobileHome.tsx
@@ -29,11 +29,11 @@ function MobileHome() {
 
     return(<>
         <div className="fixed min-h-screen bg-black top-0 bottom-0 right-0 left-0 -z-4"></div>
-        <div className="flex flex-col w-full  overflow-x-hidden">
+        <div className="flex flex-col w-full overflow-x-hidden">
             <div className="h-125 flex flex-col gap-2 pb-10 justify-end font-[Mazzard] text-white">
                 <div className="-z-1 bg-gradient-to-t from-black via-black/90 to-transparent h-full w-full fixed top-0 bottom-0 left-0 right-0"></div>
                 {projects[activeId].video == null && (
-                    <img src={`/img/${projects[activeId].title.replaceAll("/","")}.png`} className="-z-2 fixed  bottom-0 h-full w-full object-cover"></img>
+                    <img src={`/img/${projects[activeId].title.replaceAll("/","")}.png`} className="-z-2 fixed bottom-0 h-full w-full object-cover"></img>
                 )}
                 {projects[activeId].video != null && (
                     <video key={projects[activeId].video} ref={videoRef} className="-z-2 h-full w-full fixed top-0 bottom-0 left-0 right-0 object-cover" muted={isMuted} autoPlay playsInline>
