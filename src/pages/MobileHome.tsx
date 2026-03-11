23ff24eafb124f0b012cf12da79a709a6afddcbb
Keoni <keonihan@gmail.com>
2026-03-10 20:39:45 -1000

    removed vh's

--- src/pages/MobileHome.tsx
+++ src/pages/MobileHome.tsx
@@ -28,15 +28,15 @@ function MobileHome() {
     };
 
     return(<>
-        <div className="fixed min-h-dvh bg-black inset-0 top-0 bottom-0 right-0 left-0 -z-4"></div>
-        <div className="flex inset-0 flex-col w-full overflow-x-hidden">
+        <div className="fixed bg-black inset-0 top-0 bottom-0 right-0 left-0 -z-4"></div>
+        <div className="flex  flex-col w-full overflow-x-hidden">
             <div className="h-125 flex flex-col gap-2 pb-10 justify-end font-[Mazzard] text-white">
                 <div className="-z-1 bg-gradient-to-t from-black via-black/90 to-transparent h-full w-full fixed top-0 bottom-0 left-0 right-0"></div>
                 {projects[activeId].video == null && (
-                    <img src={`/img/${projects[activeId].title.replaceAll("/","")}.png`} className="-z-2 fixed bottom-0  w-full object-cover min-h-dvh"></img>
+                    <img src={`/img/${projects[activeId].title.replaceAll("/","")}.png`} className="-z-2 fixed bottom-0  w-full object-cover h-full"></img>
                 )}
                 {projects[activeId].video != null && (
-                    <video key={projects[activeId].video} ref={videoRef} className="-z-2 min-h-dvh w-full fixed top-0 bottom-0 left-0 right-0 object-cover" muted={isMuted} autoPlay playsInline>
+                    <video key={projects[activeId].video} ref={videoRef} className="-z-2 h-full w-full fixed top-0 bottom-0 left-0 right-0 object-cover" muted={isMuted} autoPlay playsInline>
                         <source src={`/video/${projects[activeId].video}.mp4`} type="video/mp4" />
                         Your browser does not support the video tag.
                     </video>
